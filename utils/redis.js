7a4fe6a0f6c86100eab2d93be08ffd13a954b9ff
Benjamin Atsu <Bredrenbonish@gmail.com>
2024-05-11 16:30:26 +0000

    Create redis constructor class

---
+++ utils/redis.js
@@ -0,0 +1,45 @@
+import { createClient } from "redis";
+
+
+class RedisClient {
+    constructor() {
+        this.client = createClient();
+        this.client.on('error', (err) => {
+            console.log('Redis Client Error:', err);
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        try {
+            const value = await this.client.get(key);
+            return value;
+        } catch(error) {
+            console.error('Error getting key: ', key, error);
+            return null;
+        }
+    }
+
+    async set(key, value, duration) {
+        try {
+            await this.client.set(key, value, 'EX', duration);
+        } catch(error) {
+            console.error('Error setting value: ', key, error);
+        }
+    }
+
+    async del(key) {
+        try {
+            await this.client.del(key);
+        } catch(error) {
+            console.error('Error deleting value: ', key, error);
+        }
+    }
+}
+
+const redisClient = new RedisClient;
+
+module.exports = redisClient;

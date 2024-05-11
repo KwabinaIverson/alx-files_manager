d7f07362ecee28b86c601ced94e6da186cb73438
Benjamin Atsu <Bredrenbonish@gmail.com>
2024-05-11 20:46:45 +0000

    Create DBCliet clas

---
+++ utils/db.js
@@ -0,0 +1,41 @@
+import { MongoClient } from 'mongodb';
+
+
+const HOST = process.env.DB_HOST || localhost;
+const PORT = process.env.DB_PORT || 27017;
+const DATABASE = process.env.DB_DATABASE || 'file_manager';
+const db_url = `mongodb://${HOST}:${PORT}`;
+
+class DBClient {
+    constructor () {
+        this.client = new MongoClient(db_url, {useUnifiedTopology: true, useNewUrlParser: true});
+        this.client.connect().then(() => {
+            this.db = this.client.db(`${DATABASE}`);
+          }).catch((err) => {
+            console.log(err);
+          });
+    }
+
+    isAlive() {
+        if (this.client.isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    async nbUsers() {
+        const users = this.db.collection('users');
+        const usersNum = await users.countDocuments();
+        return usersNum;
+      }
+    
+    async nbFiles() {
+        const files = this.db.collection('files');
+        const filesNum = await files.countDocuments();
+        return filesNum;
+    }
+}
+
+const dbClient = new DBClient;
+module.exports = dbClient;

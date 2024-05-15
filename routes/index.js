95e3f9c72f2f6b80a088a7f1fb659cf8af31db27
Benjamin Atsu <Bredrenbonish@gmail.com>
2024-05-15 18:18:40 +0000

    Add Users route to index

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
+const UserController = require('../controllers/UsersController');
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
+
 router.get('/stats', AppController.getStats);
+
+router.get('/users', UserController.postNew);
 
 
 module.exports = router;

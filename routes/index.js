ce7eb9cf03190532fdb18142ac61178551aa9008
Benjamin Atsu <Bredrenbonish@gmail.com>
2024-05-16 01:46:08 +0000

    Fix bug

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,6 @@ router.get('/connect', AuthController.getConnect);
 
 router.get('/disconnect', AuthController.getDisconnect);
 
-router.get('/users/me', UsersController.getMe);
+router.get('/users/me', UserController.getMe);
 
 module.exports = router;
